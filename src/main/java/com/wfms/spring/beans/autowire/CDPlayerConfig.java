4466018b036bbefec2464269687b26ef1ea77f57
Steven Chen <jeepchenup@foxmail.com>
2018-06-08 20:38:00 +0800

    update

--- src/main/java/com/wfms/spring/beans/autowire/CDPlayerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.wfms.spring.beans.autowire;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan
-public class CDPlayerConfig {
-}
--- src/main/java/com/wfms/spring/beans/javaconfig/CompactDisc.java
+++
@@ -1,6 +0,0 @@
-package com.wfms.spring.beans.javaconfig;
-
-public interface CompactDisc {
-
-	void play(String from);
-}
